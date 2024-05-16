598f6410fcb8f66d1b0d441499008b5e2942be55
usamafayaz <usama.fayyaz157@gmail.com>
2024-05-16 12:26:07 -0700

    Employee Summary Screen Completed and deleted unneccesary packages

--- src/screens/EmployeeSummary.jsx
+++ src/screens/EmployeeSummary.jsx
@@ -1,76 +1,69 @@
-import React, {useState} from 'react';
-import {Button, Modal, StyleSheet, TouchableOpacity, View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, View, TouchableOpacity} from 'react-native';
 import {Text} from 'react-native-paper';
 import * as Progress from 'react-native-progress';
 import PrimaryAppBar from '../components/PrimaryAppBar';
-import DatePicker from 'react-native-modern-datepicker';
+import MonthPicker from 'react-native-month-year-picker';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const EmployeeSummary = () => {
-  const [showModal, setShowModal] = useState(false);
-  const [fromDate, setFromDate] = useState(null);
-  const [toDate, setToDate] = useState(null);
-  const [activePicker, setActivePicker] = useState(null);
+  const [date, setDate] = useState(new Date());
+  const [pickerVisibility, setPickerVisibility] = useState(false);
+  const [displayedMonthYear, setDisplayedMonthYear] = useState('');
 
-  const handleFromDateChange = date => {
-    setFromDate(date);
+  useEffect(() => {
+    setDisplayedMonthYear(getDisplayedMonthYear(date));
+  }, [date]);
+
+  const showPicker = () => setPickerVisibility(true);
+
+  const onValueChange = (event, newDate) => {
+    const selectedDate = new Date(date);
+    selectedDate.setMonth(newDate.getMonth());
+    selectedDate.setFullYear(newDate.getFullYear());
+    setPickerVisibility(false);
+    setDate(selectedDate);
   };
 
-  const handleToDateChange = date => {
-    setToDate(date);
-  };
-
-  const handleSaveDate = () => {
-    // Do something with the selected dates, like save them to state or perform other actions
-    console.log('From Date:', fromDate);
-    console.log('To Date:', toDate);
-
-    // Close the modal
-    setShowModal(false);
-  };
-
-  const openDatePicker = pickerType => {
-    setActivePicker(pickerType);
-    setShowModal(true);
+  const getDisplayedMonthYear = date => {
+    const months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ];
+    const month = months[date.getMonth()];
+    const year = date.getFullYear();
+    return `${month} ${year}`;
   };
 
   return (
     <View style={styles.container}>
-      <PrimaryAppBar text={'Muhammad Anees'} />
-
+      <PrimaryAppBar text={'Summary'} />
       <View style={{paddingHorizontal: 20}}>
-        <Text style={styles.headerText}>Summary</Text>
-        <Text style={styles.label}>From:</Text>
-        <TouchableOpacity
-          onPress={() => openDatePicker('from')}
-          style={styles.row}>
-          <Icon
-            name="calendar-month"
-            size={20}
-            color="black"
-            style={styles.icon}
-          />
-          <Text style={styles.dateStyle}>
-            {fromDate ? fromDate : 'Select Date'}
-          </Text>
+        <Text style={styles.label}>Select Month:</Text>
+        <TouchableOpacity style={styles.dateRow} onPress={showPicker}>
+          <Icon name="calendar-month" color={'#000000'} size={30} />
+          <Text style={styles.dateStyle}>{displayedMonthYear}</Text>
+          <Icon name="arrow-right" color={'#000000'} size={30} />
         </TouchableOpacity>
-        <View style={styles.horizontalLineStyle}></View>
-
-        <Text style={styles.label}>To:</Text>
-        <TouchableOpacity
-          onPress={() => openDatePicker('to')}
-          style={styles.row}>
-          <Icon
-            name="calendar-month"
-            size={20}
-            color="black"
-            style={styles.icon}
+        {pickerVisibility && (
+          <MonthPicker
+            onChange={onValueChange}
+            value={date}
+            minimumDate={new Date(2000, 1)}
+            maximumDate={new Date(2025, 5)}
+            locale="en"
           />
-          <Text style={styles.dateStyle}>
-            {toDate ? toDate : 'Select Date'}
-          </Text>
-        </TouchableOpacity>
-
+        )}
         <View style={styles.horizontalLineStyle}></View>
 
         <View style={styles.progessContainer}>
@@ -100,28 +93,6 @@ const EmployeeSummary = () => {
           <Text style={styles.amountStyle}>5</Text>
         </View>
       </View>
-
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={showModal}
-        onRequestClose={() => setShowModal(false)}>
-        <View style={styles.modalContainer}>
-          <View style={styles.modalContent}>
-            <Text style={styles.modalTitle}>Select Date</Text>
-            <DatePicker
-              mode="date"
-              selected={activePicker === 'from' ? fromDate : toDate}
-              onDateChange={
-                activePicker === 'from'
-                  ? handleFromDateChange
-                  : handleToDateChange
-              }
-            />
-            <Button title="Set Date" onPress={handleSaveDate} />
-          </View>
-        </View>
-      </Modal>
     </View>
   );
 };
@@ -131,67 +102,40 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'white',
   },
-  headerText: {
-    fontSize: 19,
-    fontWeight: 'bold',
-    marginBottom: 10,
-    marginLeft: '8%',
-    marginTop: '5%',
-    color: 'black',
-  },
-  row: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginVertical: '4%',
-    paddingLeft: '25%',
-  },
-  label: {fontSize: 18, marginRight: 10, color: '#4C4C4C'},
-  icon: {marginRight: 10},
-  dateStyle: {fontSize: 18, fontWeight: '700', marginLeft: 10, color: 'black'},
-  arrow: {marginLeft: 20},
+  label: {fontSize: 18, color: '#4C4C4C', marginTop: '12%'},
   horizontalLineStyle: {
     alignSelf: 'center',
-    width: '68%',
+    width: '58%',
     backgroundColor: 'grey',
     marginTop: '2%',
-    height: 1.5,
+    height: 0.8,
     opacity: 0.5,
   },
   progessContainer: {alignItems: 'center', marginVertical: '10%'},
   bottomContainer: {
     marginBottom: '6%',
     width: '100%',
-    height: '11%',
+    height: '13%',
     alignSelf: 'center',
     alignItems: 'center',
     justifyContent: 'center',
     shadowColor: 'black',
     elevation: 1,
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
-    borderBottomLeftRadius: 20,
-    borderBottomRightRadius: 20.01,
+    borderRadius: 20,
   },
   headingStyle: {fontSize: 18, fontWeight: '700', color: '#4E4E4E'},
   amountStyle: {fontSize: 20, fontWeight: '900', color: 'black'},
-  modalContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  },
-  modalContent: {
-    backgroundColor: 'white',
-    padding: 20,
-    borderRadius: 10,
-    width: '80%',
-    elevation: 5,
-  },
-  modalTitle: {
+  dateStyle: {
     fontSize: 20,
     fontWeight: 'bold',
+    marginHorizontal: 20,
+  },
+  dateRow: {
+    marginTop: 30,
     marginBottom: 10,
-    textAlign: 'center',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
